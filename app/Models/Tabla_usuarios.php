726e0cc04fbdaf5c8e181d6d24ade7983c79e27c
AngelesZP <149276705+SandraZP@users.noreply.github.com>
2024-04-11 19:21:53 -0600

    update
    b

--- app/Models/Tabla_usuarios.php
+++ app/Models/Tabla_usuarios.php
@@ -6,8 +6,26 @@ use CodeIgniter\Model;
 
 class Tabla_usuarios extends Model
 {
-    protected $table      = ''; 
-    protected $primaryKey = ''; 
+    protected $table      = 'usuario'; 
+    protected $primaryKey = 'idusuario'; 
     protected $returnType = ''; 
-    protected $allowedFields = []; 
-}
+    protected $allowedFields = [
+        'estatus_usuario','idusuario','nombre', 'apellido_paterno','apellido_materno',
+         'sexo','correo','password','imagen_perfil', 'idrol'
+    ]; 
+
+    //====================================
+    //consultas espceificas o basicas
+    //===================================
+    //create  Read Update Delete
+    public function create($data =array()){
+       if(!empty($data)){
+
+       }//end if
+       else{
+
+       }//end else
+
+    } //end function
+    
+}//end class
