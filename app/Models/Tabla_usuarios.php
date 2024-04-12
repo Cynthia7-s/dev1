9770f348f235003684b5374b1813332d1d8d3d42
AngelesZP <149276705+SandraZP@users.noreply.github.com>
2024-04-11 19:44:12 -0600

    Tabla_usuarios
    Crud

--- app/Models/Tabla_usuarios.php
+++ app/Models/Tabla_usuarios.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use Config\Database;
 
 class Tabla_usuarios extends Model
 {
@@ -18,14 +19,47 @@ class Tabla_usuarios extends Model
     //consultas espceificas o basicas
     //===================================
     //create  Read Update Delete
-    public function create($data =array()){
+    public function create_data($data =array()){
        if(!empty($data)){
+         return $this
+               ->table($this->table)
+               ->insert($data);
 
        }//end if
        else{
-
+             return FALSE;
        }//end else
 
-    } //end function
+    } //end create
+
+    public function get_user($constraints = array()){
+        return $this
+        ->table($this->table)
+        ->where($constraints)
+         ->get()
+      ->getRow();
+    }//end get user
+
+
+    public function get_table(){
+        return $this
+        ->table($this->table)
+         ->get()
+      ->getResult();
+    }//end get_table
     
+    public function update_data($id=0, $data= array()){
+              if(!empty($data)){
+        
+                   return $this
+                  ->table($this->table)
+                  ->where([$this->primaryKey=>$id])
+                  ->set($data)
+                 ->update();
+               }
+               else{
+                return FALSE;
+               }
+    }//end get_table
+
 }//end class
